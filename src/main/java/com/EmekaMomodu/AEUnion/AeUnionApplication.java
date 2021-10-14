30466a9d9017931e1041f28075324b7e67a8efaf
Chukwuemeka Momodu <cmomodu@ecobank.com>
2021-10-14 19:53:03 +0100

    first commit

---
+++ src/main/java/com/EmekaMomodu/AEUnion/AeUnionApplication.java
@@ -0,0 +1,13 @@
+package com.EmekaMomodu.AEUnion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AeUnionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AeUnionApplication.class, args);
+	}
+
+}
