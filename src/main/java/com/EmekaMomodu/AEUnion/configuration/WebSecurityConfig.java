923958da750435b810b8453431f7c2c7168eb934
Chukwuemeka Momodu <momodu.ceo@gmail.com>
2021-10-21 00:42:52 +0100

    redirect http to https

---
+++ src/main/java/com/EmekaMomodu/AEUnion/configuration/WebSecurityConfig.java
@@ -0,0 +1,20 @@
+package com.EmekaMomodu.AEUnion.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author CMOMODU
+ * @version 1.0
+ * @date 10/21/21 12:38 AM
+ */
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.requiresChannel()
+                .requestMatchers(r -> r.getHeader("X-Forwarded-Proto") != null)
+                .requiresSecure();
+    }
+}
