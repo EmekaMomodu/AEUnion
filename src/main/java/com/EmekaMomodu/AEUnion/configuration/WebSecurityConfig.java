658151021214068634514e2acf787058be5f33d6
Chukwuemeka Momodu <momodu.ceo@gmail.com>
2023-04-07 13:43:54 -0400

    removed redirects for strict https, already handled by gcp outside the container

--- src/main/java/com/EmekaMomodu/AEUnion/configuration/WebSecurityConfig.java
+++ src/main/java/com/EmekaMomodu/AEUnion/configuration/WebSecurityConfig.java
@@ -13,8 +13,8 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.requiresChannel()
-                .requestMatchers(r -> r.getHeader("X-Forwarded-Proto") != null)
-                .requiresSecure();
+//        http.requiresChannel()
+//                .requestMatchers(r -> r.getHeader("X-Forwarded-Proto") != null)
+//                .requiresSecure();
     }
 }
